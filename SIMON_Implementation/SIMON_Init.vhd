c3d369db55b6370d81814d445aa9c5d268753381
areebTP <92602684+areebTP@users.noreply.github.com>
2022-04-15 13:34:48 +0100

    Testbench tested version

--- SIMON_Implementation/SIMON_Init.vhd
+++ SIMON_Implementation/SIMON_Init.vhd
@@ -10,7 +10,7 @@ port
  
 	key_length : in std_logic_vector(1 downto 0); -- Set "00" to 128 bit, "01" to 192 bit, "10" to 256 bit
 	key_valid : in std_logic; -- enable intialisation = '1', disable = '0'
-	key_word_in : inout std_logic_vector(31 downto 0); -- key for initialising the encyrption algorithm 
+	key_word_in : in std_logic_vector(31 downto 0); -- key for initialising the encyrption algorithm 
 	
 	nrSubkeys : out integer -- used for the next stage: encryption
 );
@@ -22,8 +22,8 @@ architecture rtl of SIMON_Init is
 type t_subkeys is array(0 to 71) of unsigned(63 downto 0);	
 signal subkeys : t_subkeys;
 
-signal sub_key_first : unsigned(31 downto 0) := (others=> '0');
-signal sub_key_second : unsigned(31 downto 0) := (others=> '0');
+--signal sub_key_first : unsigned(31 downto 0) := (others=> '0');
+--signal sub_key_second : unsigned(31 downto 0) := (others=> '0');
 
 type t_key_64bit is array(0 to 2) of unsigned(63 downto 0);
 signal key_64bit : t_key_64bit; -- array of 2 length for storing 2 keys 64-bit long
@@ -37,46 +37,47 @@ begin
 	return unsigned(shifted);
 end function;
 -----------------------------------------------------------------------------
-
+--signal i : integer:=0;
 begin
 	
-	process(clk) is
+	process(clk,key_valid) is
 		variable i: integer := 0;
+		variable sub_key_first : unsigned(31 downto 0) := (others=> '0');
+		variable sub_key_second : unsigned(31 downto 0) := (others=> '0');
 	begin
 		
 		if (rising_edge(clk) and key_valid = '1') then -- only begin if key_valid = '1'
 			
-			
-			if i = 6 then -- reset i variable
-				i := 0;
-			end if;
-			
-			case i is 
+				if i = 6 then -- reset i signal 
+					i := 0;
+				end if;
 				
-				when 0 =>sub_key_first <= unsigned(key_word_in);
-				when 1 => -- store into the first element of the array 
-					sub_key_second <= unsigned(key_word_in);
-					key_64bit(0) <=  unsigned(sub_key_second) & unsigned(sub_key_first); -- store 64-bit key (second<<31 | first)
+				case i is 
 					
-				when 2=>sub_key_first <= unsigned(key_word_in); 
-				when 3=> -- store into the second element of the array
-					sub_key_second <= unsigned(key_word_in);
-					key_64bit(1) <= unsigned(sub_key_second) & unsigned(sub_key_first); -- store 64-bit key (second<<31 | first)				
+					when 0 =>sub_key_first := unsigned(key_word_in);
+					when 1 => -- store into the first element of the array 
+						sub_key_second := unsigned(key_word_in);
+						key_64bit(0) <=  unsigned(sub_key_second) & unsigned(sub_key_first); -- store 64-bit key (second<<31 | first)
+						
+					when 2=>sub_key_first := unsigned(key_word_in); 
+					when 3=> -- store into the second element of the array
+						sub_key_second := unsigned(key_word_in);
+						key_64bit(1) <= unsigned(sub_key_second) & unsigned(sub_key_first); -- store 64-bit key (second<<31 | first)				
+						
+					when 4=>sub_key_first := unsigned(key_word_in);
+						
+					when 5=>-- store into the second element of the array
+						sub_key_second := unsigned(key_word_in);
+						key_64bit(2) <= unsigned(sub_key_second) & unsigned(sub_key_first); -- store 64-bit key (second<<31 | first)
+						
+					when others=>
+						null; -- do nothing 
 					
-				when 4=>sub_key_first <= unsigned(key_word_in);
-					
-				when 5=>-- store into the second element of the array
-					sub_key_second <= unsigned(key_word_in);
-					key_64bit(2) <= unsigned(sub_key_second) & unsigned(sub_key_first); -- store 64-bit key (second<<31 | first)
-					
-				when others=>
-					null; -- do nothing 
-				
-			end case;
-
-			i := i+1;-- increment 
+				end case;
+	
+				i := i+1;-- increment 
 			
-		end if;
+		end if;-- if for rising_edge clock 
 	end process;
 	
 		
@@ -85,6 +86,7 @@ begin
 		variable c : unsigned(63 downto 0):= x"fffffffffffffffc"; 
 		variable z : unsigned(63 downto 0);
 		variable i : unsigned(63 downto 0);
+		variable one : unsigned(63 downto 0):= B"0000000000000000000000000000000000000000000000000000000000000001";
 		
 	begin 
 	
@@ -106,11 +108,11 @@ begin
 							subkeys(0) <= key_64bit(1);
 							
 							for i in 2 to 66 loop
-								subkeys(i) <= (c xor (z and B"01") xor subkeys(i-2) xor ROR_64(subkeys(i-1),3) xor ROR_64(subkeys(i-1),4));
+								subkeys(i) <= (c xor (z and one) xor subkeys(i-2) xor ROR_64(subkeys(i-1),3) xor ROR_64(subkeys(i-1),4));
 								z:= shift_right(z,1);
 							end loop;
 							
-							subkeys(66) <= (c xor B"01" xor subkeys(64) xor ROR_64(subkeys(65),3) xor ROR_64(subkeys(65),4));
+							subkeys(66) <= (c xor one xor subkeys(64) xor ROR_64(subkeys(65),3) xor ROR_64(subkeys(65),4));
 							subkeys(67) <= (c xor subkeys(65) xor ROR_64(subkeys(66),3) xor ROR_64(subkeys(66),4) );
 							
 						elsif key_length = "01" then -- key length is 192-bit
@@ -123,12 +125,12 @@ begin
 							subkeys(2) <= key_64bit(0);
 							
 							for i in 3 to 67 loop
-								subkeys(i) <= (c xor (z and B"01") xor subkeys(i-2) xor ROR_64(subkeys(i-1),3) xor ROR_64(subkeys(i-1),4) );
+								subkeys(i) <= (c xor (z and one) xor subkeys(i-2) xor ROR_64(subkeys(i-1),3) xor ROR_64(subkeys(i-1),4) );
 								z:= shift_right(z,1);
 							end loop;
 							
 							subkeys(67) <=	(c xor subkeys(64) xor ROR_64(subkeys(66), 3) xor ROR_64(subkeys(66), 4) );
-							subkeys(68) <= (c xor B"01" xor subkeys(65) xor ROR_64(subkeys(66),3) xor ROR_64(subkeys(66),4) );
+							subkeys(68) <= (c xor one xor subkeys(65) xor ROR_64(subkeys(66),3) xor ROR_64(subkeys(66),4) );
 							
 						end if;
 				
