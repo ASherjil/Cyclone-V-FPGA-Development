f72860774afecd2380d89050554cbfa28661691a
areebTP <92602684+areebTP@users.noreply.github.com>
2022-04-30 13:50:32 +0100

    updated with a working copy

--- SIMON_Implementation/SIMON_FINAL/Cortex_A9_Interfacing/Simon.h
+++ SIMON_Implementation/SIMON_FINAL/Cortex_A9_Interfacing/Simon.h
@@ -26,4 +26,13 @@ void SIMON_init(SimonContext* context, uint64_t* key, uint16_t keyLen);
 void SIMON_encrypt(SimonContext* context, uint64_t* block, uint64_t* out);
 void SIMON_decrypt(SimonContext* context, uint64_t* block, uint64_t* out);
 
+//------------------------------------------------FPGA Interfacing functions
+int FPGA_decrypt(uint64_t*,uint64_t*,uint64_t*);
+//-----------------------------------------------linux kernal functions----
+int open_physical(int);
+void* map_physical(int, unsigned int, unsigned int);
+void close_physical(int);
+int unmap_physical(void*, unsigned int);
+//--------------------------------------------------------------------------
+
 void SIMON_main(void);
