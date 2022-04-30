450bc30a389c1adfcfc431a1ae70d4d9d2f02d90
areebTP <92602684+areebTP@users.noreply.github.com>
2022-04-30 15:11:09 +0100

    Testbench for both 128-bit and 192-bit keys

--- SIMON_Implementation/SIMON_FINAL/Hardware_dual_keys_tb.vhd
+++ SIMON_Implementation/SIMON_FINAL/Hardware_dual_keys_tb.vhd
@@ -1,3 +1,4 @@
+--------------------------------------
 -- Library
 --------------------------------------
 library IEEE;
@@ -42,6 +43,7 @@ begin
 
   STIM: process
   begin
+ -----------------------------------------------------------------Check #1 for 192-bit key legnth 
 	avs_s0_write <= '1'; -- enable write(take input for HPS) 
 	avs_s0_read <= '0';
 	wait for clk_period;
@@ -107,10 +109,73 @@ begin
 	avs_s0_address <= B"01001"; -- data_word_out signal address = 0x9
     wait for clk_period;
     avs_s0_address <= B"01010"; -- data_word_out signal address = 0xA
+	
+-------------------------------------------------------Now check for 128-bit key length 
+
+	wait for 500 ns;-- create time gap 
+	avs_s0_write <= '1'; -- enable write(take input for HPS) 
+	avs_s0_read <= '0';
+	wait for clk_period;
+	avs_s0_address <= B"01011"; -- reset_n signal address = 0xB
+    avs_s0_writedata  <= (others=> '0');  -- set to reset_n = '0'
+	wait for clk_period;
+	avs_s0_writedata  <= B"00000000000000000000000000000001";  -- set to reset_n = '1'
+	wait for clk_period;
+	avs_s0_address <= B"00011"; -- encryption signal address = 0x3
+    avs_s0_writedata  <= (others=> '0');  -- Set to Encryption to '0'= Decryption
+	wait for clk_period; -- wait for 20 ns 
+	avs_s0_address <= B"00000"; -- key_length signal address = 0x0
+    avs_s0_writedata <= B"00000000000000000000000000000000"; -- Set "00" to 128 bit key_length 
+	wait for clk_period;
+	avs_s0_address <= B"00101"; -- key_word_in signal address = 0x5
+    avs_s0_writedata  <= x"89abcdef";  -- 1, key_word_in <=  {0x01234567|89abcdef,0x0fedcba9|87456321}										
+	wait for clk_period;
+	avs_s0_address <= B"01100"; -- key_word_in signal address = 0xC
+	avs_s0_writedata  <= x"01234567"; -- 2
+    wait for clk_period;
+	avs_s0_address <= B"01101"; -- key_word_in signal address = 0xD
+    avs_s0_writedata  <= x"87456321"; -- 3
+    wait for clk_period;
+	avs_s0_address <= B"01110"; -- key_word_in signal address = 0xE
+    avs_s0_writedata  <= x"0fedcba9"; -- 4
+	wait for clk_period; -- 20 ns 
+	avs_s0_address <= B"00001"; -- key_valid signal address = 0x1
+    avs_s0_writedata    <= B"00000000000000000000000000000001"; -- key_valid <= '1';
+    wait for clk_period;
+    avs_s0_writedata    <= B"00000000000000000000000000000000"; -- key_valid <= '0';
+	wait for clk_period;
+	avs_s0_address <= B"00100"; -- data_word_in signal address = 0x4
+    avs_s0_writedata  <= x"32713d11"; -- data_word_in <= {0x2c52530e|32713d11, 0x548e625f|df9bf565}
+	wait for clk_period;
+	avs_s0_address <= B"10001"; -- data_word_in signal address = 0x11
+    avs_s0_writedata <= x"2c52530e"; --2 
+    wait for clk_period;
+	avs_s0_address <= B"10010"; -- data_word_in signal address = 0x12
+    avs_s0_writedata <= x"df9bf565";-- 3 
+    wait for clk_period;
+	avs_s0_address <= B"10011"; -- data_word_in signal address = 0x13
+    avs_s0_writedata <= x"548e625f";-- 4
+    wait for clk_period;
+	avs_s0_address <= B"00010"; -- data_valid signal address = 0x2
+    avs_s0_writedata    <= B"00000000000000000000000000000001"; -- data_valid <= '1';
+    wait for clk_period;
+    avs_s0_writedata    <= B"00000000000000000000000000000000"; -- data_valid <= '0';
+	wait for clk_period;
+	avs_s0_read <= '1';
+	avs_s0_address <= B"00110"; -- data_ready signal address = 0x6
+    wait until avs_s0_readdata(0) = '1'; -- wait until data_ready = '1'
+    wait for clk_period;
+	avs_s0_address <= B"00111"; -- data_word_out signal address = 0x7
+    wait for clk_period;
+	avs_s0_address <= B"01000"; -- data_word_out signal address = 0x8
+    wait for clk_period;
+	avs_s0_address <= B"01001"; -- data_word_out signal address = 0x9
+    wait for clk_period;
+    avs_s0_address <= B"01010"; -- data_word_out signal address = 0xA
 ---------------------------------------------------------------------------
--- Check for restarting 
+-- Check #2 for 192-bit key length(another check for 192-bit key )  
 		
-	wait for 500 ns;
+	wait for 500 ns; -- create time gap 
 	avs_s0_write <= '1'; -- enable write(take input for HPS) 
 	avs_s0_read <= '0';
 	wait for clk_period;
@@ -177,75 +242,8 @@ begin
     wait for clk_period;
     avs_s0_address <= B"01010"; -- data_word_out signal address = 0xA
 	
--------------------------------------------------------Check for restarting 
-	wait for 500 ns;
-	avs_s0_write <= '1'; -- enable write(take input for HPS) 
-	avs_s0_read <= '0';
-	wait for clk_period;
-	avs_s0_address <= B"01011"; -- reset_n signal address = 0xB
-    avs_s0_writedata  <= (others=> '0');  -- set to reset_n = '0'
-	wait for clk_period;
-	avs_s0_writedata  <= B"00000000000000000000000000000001";  -- set to reset_n = '1'
-	wait for clk_period;
-	avs_s0_address <= B"00011"; -- encryption signal address = 0x3
-    avs_s0_writedata  <= (others=> '0');  -- Set to Encryption to '0'= Decryption
-	wait for clk_period; -- wait for 20 ns 
-	avs_s0_address <= B"00000"; -- key_length signal address = 0x0
-    avs_s0_writedata <= B"00000000000000000000000000000001"; -- Set "01" to 192 bit
-	wait for clk_period;
-	avs_s0_address <= B"00101"; -- key_word_in signal address = 0x5
-    avs_s0_writedata  <= x"51321318";  -- 1, key_word_in <= {0x13248994|51321318,0x98481320|20789452,}
-														-- 	 {0x12354894|8784123F}
-	wait for clk_period;
-	avs_s0_address <= B"01100"; -- key_word_in signal address = 0xC
-	avs_s0_writedata  <= x"13248994"; -- 2
-    wait for clk_period;
-	avs_s0_address <= B"01101"; -- key_word_in signal address = 0xD
-    avs_s0_writedata  <= x"20789452"; -- 3
-    wait for clk_period;
-	avs_s0_address <= B"01110"; -- key_word_in signal address = 0xE
-    avs_s0_writedata  <= x"98481320"; -- 4
-    wait for clk_period;
-	avs_s0_address <= B"01111"; -- key_word_in signal address = 0xF
-	avs_s0_writedata  <= x"8784123F"; -- 5
-    wait for clk_period;
-	avs_s0_address <= B"10000"; -- key_word_in signal address = 0x10
-    avs_s0_writedata  <= x"12354894"; -- 6
-	wait for clk_period; -- 20 ns 
-	avs_s0_address <= B"00001"; -- key_valid signal address = 0x1
-    avs_s0_writedata    <= B"00000000000000000000000000000001"; -- key_valid <= '1';
-    wait for clk_period;
-    avs_s0_writedata    <= B"00000000000000000000000000000000"; -- key_valid <= '0';
-	wait for clk_period;
-	avs_s0_address <= B"00100"; -- data_word_in signal address = 0x4
-    avs_s0_writedata  <= x"f1849c38"; -- data_word_in <=  {  0x776b8923|f1849c38, 0x16d57b89|c656b268}
-	wait for clk_period;
-	avs_s0_address <= B"10001"; -- data_word_in signal address = 0x11
-    avs_s0_writedata <= x"776b8923"; --2 
-    wait for clk_period;
-	avs_s0_address <= B"10010"; -- data_word_in signal address = 0x12
-    avs_s0_writedata <= x"c656b268";-- 3 
-    wait for clk_period;
-	avs_s0_address <= B"10011"; -- data_word_in signal address = 0x13
-    avs_s0_writedata <= x"16d57b89";-- 4
-    wait for clk_period;
-	avs_s0_address <= B"00010"; -- data_valid signal address = 0x2
-    avs_s0_writedata    <= B"00000000000000000000000000000001"; -- data_valid <= '1';
-    wait for clk_period;
-    avs_s0_writedata    <= B"00000000000000000000000000000000"; -- data_valid <= '0';
-	wait for clk_period;
-	avs_s0_read <= '1';
-	avs_s0_address <= B"00110"; -- data_ready signal address = 0x6
-    wait until avs_s0_readdata(0) = '1'; -- wait until data_ready = '1'
-    wait for clk_period;
-	avs_s0_address <= B"00111"; -- data_word_out signal address = 0x7
-    wait for clk_period;
-	avs_s0_address <= B"01000"; -- data_word_out signal address = 0x8
-    wait for clk_period;
-	avs_s0_address <= B"01001"; -- data_word_out signal address = 0x9
-    wait for clk_period;
-    avs_s0_address <= B"01010"; -- data_word_out signal address = 0xA
-	wait;
+
+	wait;-- do not loop back(stop)
 	
   end process;
 
