68ffd841ed8a6927f827b2b1a6503737f41a75d1
areebTP <92602684+areebTP@users.noreply.github.com>
2022-05-08 19:18:53 +0100

    Tb for both 128-bit and 192-bit keys

--- SIMON_Implementation/SIMON_tb.vhd
+++ SIMON_Implementation/SIMON_tb.vhd
@@ -1,3 +1,4 @@
+-- Dual testbench for 128-bit keys and 192-bit keys 
 --------------------------------------
 -- Library
 --------------------------------------
@@ -7,13 +8,13 @@ use IEEE.std_logic_1164.all;
 --------------------------------------
 -- Entity
 --------------------------------------
-entity tb is
+entity key_192_tb is
 end entity;
 
 --------------------------------------
 -- Architecture
 --------------------------------------
-architecture tb of tb is
+architecture sim of key_192_tb is
   -- Clock definition
   constant clk_period : time := 20 ns;
 
@@ -35,7 +36,7 @@ architecture tb of tb is
 begin
   clk <= not clk after clk_period/2;
 
-  DUT: entity work.simon_top
+  DUT: entity work.Simon_Init
   port map(
     clk           => clk,
     reset_n       => reset_n,
@@ -51,16 +52,17 @@ begin
 
   STIM: process
   begin
-    reset_n      <= '0';
+----------------------------------------------------------------TEST #1 FOR 128-BIT KEY 
+	reset_n      <= '0';
     wait for 4*clk_period;
     encryption   <= '1';  -- Set to Encryption 
     key_length   <= "00"; -- Set "00" to 128 bit, "01" to 192 bit, "10" to 256 bit
     reset_n      <= '1';
     wait for clk_period;
     key_valid    <= '1';
-    key_word_in  <= x"DEADBEEF"; 
+    key_word_in  <= x"DEADBEEF";
     wait for clk_period;
-    key_word_in  <= x"01234567"; 
+    key_word_in  <= x"01234567";
     wait for clk_period;
     key_word_in  <= x"89ABCDEF";
     wait for clk_period;
@@ -88,7 +90,7 @@ begin
     data_bkp(63 downto 32)  <= data_word_out;
     wait for clk_period;
     data_bkp(31 downto 0)   <= data_word_out;
-    -----------------------------------------------------------------
+
     wait for 4*clk_period;
     reset_n      <= '0';
     wait for clk_period;
@@ -127,7 +129,94 @@ begin
     data_bkp(63 downto 32)  <= data_word_out;
     wait for clk_period;
     data_bkp(31 downto 0)   <= data_word_out;
-    wait;
+	
+----------------------------------------------------------------TEST #2 FOR 192-BIT KEY 
+	wait for clk_period;
+    reset_n      <= '0';
+    wait for 4*clk_period; -- 20 ns * 4 = 80 ns
+    encryption   <= '1';  -- Set to Encryption 
+    key_length   <= "01"; -- Set "00" to 128 bit, "01" to 192 bit, "10" to 256 bit
+    reset_n      <= '1';
+    wait for clk_period; -- 20 ns 
+    key_valid    <= '1';
+    key_word_in  <= x"DEADBEEF";  -- 1
+    wait for clk_period;
+    key_word_in  <= x"01234567"; -- 2
+    wait for clk_period;
+    key_word_in  <= x"89ABCDEF"; -- 3
+    wait for clk_period;
+    key_word_in  <= x"DEADBEEF"; -- 4
+    wait for clk_period;
+	key_word_in  <= x"BEEF1ABC"; -- 5
+    wait for clk_period;
+    key_word_in  <= x"ABCDEF01"; -- 6
+    wait for clk_period;
+    key_valid    <= '0';
+    key_word_in  <= x"00000000"; -- 7 {key_word_in}
+    data_valid   <= '1';
+    data_word_in <= x"A5A5A5A5";  -- 1
+    wait for clk_period;
+    data_word_in <= x"01234567"; --2 
+    wait for clk_period;
+    data_word_in <= x"FEDCBA98";-- 3
+    wait for clk_period;
+    data_word_in <= x"5A5A5A5A";-- 4
+    wait for clk_period;
+    data_valid   <= '0';
+    data_word_in <= x"00000000";-- 5 {data_word_in}
+    wait until data_ready = '1';
+    wait for clk_period;
+    data_bkp(127 downto 96) <= data_word_out; -- 1
+    wait for clk_period;
+    data_bkp(95 downto 64)  <= data_word_out;-- 2
+    wait for clk_period;
+    data_bkp(63 downto 32)  <= data_word_out;-- 3
+    wait for clk_period;
+    data_bkp(31 downto 0)   <= data_word_out;-- 4 {encypted_text, uint64_t arr[2]}
+	
+    wait for 4*clk_period; -- 80 ns 
+    reset_n      <= '0';
+    wait for clk_period;
+    encryption   <= '0';  -- Set to Decryption 
+    key_length   <= "01"; -- Set "00" to 128 bit, "01" to 192 bit, "10" to 256 bit
+    reset_n      <= '1';
+    wait for clk_period;
+    key_valid    <= '1';
+    key_word_in  <= x"DEADBEEF";
+    wait for clk_period;
+    key_word_in  <= x"01234567";
+    wait for clk_period;
+    key_word_in  <= x"89ABCDEF";
+    wait for clk_period;
+    key_word_in  <= x"DEADBEEF";
+	wait for clk_period;
+	key_word_in  <= x"BEEF1ABC"; -- 5
+    wait for clk_period;
+    key_word_in  <= x"ABCDEF01"; -- 6
+	wait for clk_period;
+    key_valid    <= '0';
+    key_word_in  <= x"00000000"; -- 7 {key_word_in}
+    data_valid   <= '1';
+    data_word_in <= data_bkp(127 downto 96);
+    wait for clk_period;
+    data_word_in <= data_bkp(95 downto 64);
+    wait for clk_period;
+    data_word_in <= data_bkp(63 downto 32);
+    wait for clk_period;
+    data_word_in <= data_bkp(31 downto 0);
+    wait for clk_period;
+    data_valid   <= '0';
+    data_word_in <= x"00000000";
+    wait until data_ready = '1';
+    wait for clk_period;
+    data_bkp(127 downto 96) <= data_word_out;
+    wait for clk_period;
+    data_bkp(95 downto 64)  <= data_word_out;
+    wait for clk_period;
+    data_bkp(63 downto 32)  <= data_word_out;
+    wait for clk_period;
+    data_bkp(31 downto 0)   <= data_word_out;
+    wait;-- STOP do not loop back 
   end process;
 
 end architecture;
